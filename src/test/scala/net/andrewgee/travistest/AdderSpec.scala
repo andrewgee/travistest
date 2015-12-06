8e8fcfacf08962de43f0f0b8dc92a0514a22270b
Andrew Gee <andrew@andrewgee.net>
2015-12-06 13:06:27 +0000

    Added another test

--- src/test/scala/net/andrewgee/travistest/AdderSpec.scala
+++ src/test/scala/net/andrewgee/travistest/AdderSpec.scala
@@ -7,5 +7,9 @@ class AdderSpec extends WordSpec with Matchers {
     "return 1 for 0,1" in {
       Adder.add(0,1) shouldBe 1
     }
+
+    "return 2 for 1.1" in {
+      Adder.add(1,1) shouldBe 2
+    }
   }
 }
